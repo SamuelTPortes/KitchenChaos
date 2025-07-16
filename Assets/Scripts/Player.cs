4a68bb4b52b1d3dcddc1d68ffe9c2e2d2c703ab6
Elizeu Corrêa <elizeucp01@gmail.com>
2025-07-15 23:42:10 -0300

    Create Prefab Folder for own Creation, Adapt Player Script code for Raycast function

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,14 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
 
     private bool isWalking;
 
-    private void Update() {
+    private void Update()
+    {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, moveDir, out RaycastHit raycastHit, interactDistance))
+        {
+            Debug.Log(raycastHit.transform);
+        }
+        else
+        {
+            Debug.Log("-");
+        }
+    }
+
+    private void HandleMovement()
+    {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
@@ -19,7 +50,8 @@ public class Player : MonoBehaviour {
         float playerHeight = 2f;
         bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
 
-        if (canMove) {
+        if (canMove)
+        {
             transform.position += moveDir * moveDistance;
         }
 
@@ -27,9 +59,5 @@ public class Player : MonoBehaviour {
         float rotationSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
 
-    }
-
-    public bool IsWalking() {
-        return isWalking;
     }
 }
