c6b1b9a502513a878b2edd4d69268cd2c1206b53
Samuel <samueltportes@gmail.com>
2025-07-17 20:18:19 -0300

    Interact with E

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,15 +1,25 @@
+using System;
 using Unity.Multiplayer.Center.Common.Analytics;
 using UnityEngine;
 
 public class GameInput : MonoBehaviour {
 
+
+    public event EventHandler OnInteractAction;
     private PlayerInputActions playerInputActions;
     private void Awake() {
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
 
 
-        playerInputActions.Player.Interact.performed += interact_performed;
+        playerInputActions.Player.Interact.performed += Interact_perfomed;
+    }
+
+    private void Interact_perfomed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
+        // if (OnInteractAction != null) {
+        //     OnInteractAction(this, EventArgs.Empty);
+        // }
+        OnInteractAction?.Invoke(this, EventArgs.Empty); // Mesma coisa do código de cima
     }
     public Vector2 GetMovementVectorNormalized() {
         Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
 
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
@@ -12,48 +11,67 @@ public class Player : MonoBehaviour
     private bool isWalking;
     private Vector3 lastInteractDir;
 
-    private void Update()
-    {
-        HandleMovement();
-        HandleInteractions();
+    private void Start() {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
     }
 
-    public bool IsWalking()
-    {
-        return isWalking;
-    }
-
-
-    private void HandleInteractions()
-    {
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e) {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
 
 
-        if (moveDir != Vector3.zero)
-        {
+        if (moveDir != Vector3.zero) {
             lastInteractDir = moveDir;
         }
 
 
         float interactDistance = 2f;
-        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
-        {
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask)) {
             if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
             // Tem o Clear Counter
             {
                 clearCounter.Interact();
             }
-        }
-        else
-        {
+        } else {
             Debug.Log("-");
         }
     }
 
-    private void HandleMovement()
-    {
+    private void Update() {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    public bool IsWalking() {
+        return isWalking;
+    }
+
+
+    private void HandleInteractions() {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+
+        if (moveDir != Vector3.zero) {
+            lastInteractDir = moveDir;
+        }
+
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask)) {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            // Tem o Clear Counter
+            {
+
+            }
+        } else {
+
+        }
+    }
+
+    private void HandleMovement() {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
@@ -87,8 +105,8 @@ public class Player : MonoBehaviour
                 }
             }
         }
-        
-        if (canMove){
+
+        if (canMove) {
             transform.position += moveDir * moveDistance;
         }
 
