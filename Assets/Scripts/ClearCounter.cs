80c35a129f4557fd8b8c8230344eeb48dd4f135e
Samuel <samueltportes@gmail.com>
2025-07-22 20:51:48 -0300

    KitchenObjectSO Added, Tomato and CheeseBlock too

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,18 +1,16 @@
 using UnityEngine;
 
-public class ClearCounter : MonoBehaviour
-{
+public class ClearCounter : MonoBehaviour {
 
 
-    [SerializeField] private Transform tomatoPrefab;
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
 
-    public void Interact()
-    {
+    public void Interact() {
 
         Debug.Log("Interagiu!");
-        Transform tomatoTransform = Instantiate(tomatoPrefab, counterTopPoint);
-        tomatoTransform.localPosition = Vector3.zero;
+        Transform kitcheObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+        kitcheObjectTransform.localPosition = Vector3.zero;
     }
 
 
