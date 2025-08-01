2d24669359f207d711e1e07fe811541f4bbadd75
Elizeu Corrêa do Prado <elizeucp01@gmail.com>
2025-08-01 20:10:24 -0300

    Plates Counter Fully Working

--- Assets/Scripts/Counter/PlatesCounter.cs
+++ Assets/Scripts/Counter/PlatesCounter.cs
@@ -1,13 +1,22 @@
+using System;
 using UnityEngine;
 
 public class PlatesCounter : BaseCounter
 {
 
 
+
+    public event EventHandler OnPlateSpawned;
+    public event EventHandler OnPlateRemoved;
+
     [SerializeField] private KitchenObjectSO plateKitchenObjectSO;
 
     private float spawnPlateTimer;
     private float spawnPlateTimerMax = 4f;
+    private int platesSpawnedAmount;
+    private int platesSpawnedAmountMax = 4;
+
+
 
     private void Update()
     {
@@ -15,7 +24,30 @@ public class PlatesCounter : BaseCounter
 
         if (spawnPlateTimer > spawnPlateTimerMax)
         {
-            KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, this);
+            spawnPlateTimer = 0f;
+            if (platesSpawnedAmount < platesSpawnedAmountMax)
+            {
+                platesSpawnedAmount++;
+
+                OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public override void Interact(Player player)
+    {
+        if (!player.HasKitchenObject())
+        {
+            //Jogador não está segurando nada
+            if (platesSpawnedAmount > 0)
+            {
+                //Há pelomenos um prato aqui
+                platesSpawnedAmount--;
+
+                KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, player);
+
+                OnPlateRemoved?.Invoke(this, EventArgs.Empty);
+            }
         }
     }
 
